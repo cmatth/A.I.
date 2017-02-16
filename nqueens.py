45bd0c10c715a5e8977cf6d37c85a43498016f2c
cmatth <cmatthew@uccs.edu>
2017-02-16 10:35:01 -0700

    started queens

--- nqueens.py
+++ nqueens.py
@@ -11,12 +11,18 @@ _board = 0
 def newBoard(size):
 		global _size
 		_size  = size
-		board = numpy.zeroes(size**2)
+		board = np.zeros(size**2)
 		board = board.reshape(size,size)
 
 		for y in range(0, _size):
 			board[0][y] = 1
 		return board
-end
 			
+board = newBoard(3)
+thing = np.where(board == 1)
+print thing[1][2], 
 
+def solved(board)
+		thing = np.where(board == 1)
+		for y in range(0, len(thing[0]))
+			queens = 
